8f41779d49a111dfd27304181f42d8bcce9d1fc3
sipho-mokoena <siphowilliammokoena@gmail.com>
2025-04-24 18:21:42 +0200

    feat: Refactor MainActivity, CandidateMainActivity, and EmployerMainActivity to improve navigation setup and visibility handling for bottom navigation

--- app/src/main/java/com/pbde401/studyworks/ui/candidate/CandidateMainActivity.java
+++ app/src/main/java/com/pbde401/studyworks/ui/candidate/CandidateMainActivity.java
@@ -1,37 +1,60 @@
 package com.pbde401.studyworks.ui.candidate;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import com.pbde401.studyworks.R;
 import com.pbde401.studyworks.databinding.ActivityCandidateMainBinding;
 
 public class CandidateMainActivity extends AppCompatActivity {
-
     private ActivityCandidateMainBinding binding;
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityCandidateMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_candidate_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        // Set up toolbar if needed
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        // Set up navigation
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_candidate_main);
+        
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+            
+            // Set up bottom navigation
+            BottomNavigationView bottomNav = findViewById(R.id.menu_candidate_bottom_navigation);
+            appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_candidate_dashboard,
+                R.id.navigation_candidate_jobs,
+                R.id.navigation_candidate_applications,
+                R.id.navigation_candidate_chats,
+                R.id.navigation_candidate_profile
+            ).build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            NavigationUI.setupWithNavController(bottomNav, navController);
+        }
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
--- app/src/main/java/com/pbde401/studyworks/ui/employer/EmployerMainActivity.java
+++ app/src/main/java/com/pbde401/studyworks/ui/employer/EmployerMainActivity.java
@@ -1,37 +1,55 @@
 package com.pbde401.studyworks.ui.employer;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import com.pbde401.studyworks.databinding.ActivityEmployerMainBinding;
+import com.pbde401.studyworks.R;
 
 public class EmployerMainActivity extends AppCompatActivity {
-
-    private ActivityEmployerMainBinding binding;
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer_main);
 
-        binding = ActivityEmployerMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // Set up toolbar if needed
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_employer_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        // Set up navigation
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_employer_main);
+        
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+            
+            // Set up bottom navigation
+            BottomNavigationView bottomNav = findViewById(R.id.menu_employer_bottom_navigation);
+            appBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.navigation_employer_dashboard,
+                    R.id.navigation_employer_jobs,
+                    R.id.navigation_employer_applications,
+                    R.id.navigation_employer_chats,
+                    R.id.navigation_employer_profile
+            ).build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            NavigationUI.setupWithNavController(bottomNav, navController);
+        }
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
