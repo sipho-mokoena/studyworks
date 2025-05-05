b827b9fa8fd6a09d0f8540391f1ad59c5a5a011d
sipho-mokoena <siphowilliammokoena@gmail.com>
2025-05-06 00:18:02 +0200

    feat: Add repositories for applications, chats, and candidate profiles with Firebase integration

--- app/src/main/java/com/pbde401/studyworks/ui/candidate/chats/CandidateChatsViewModel.java
+++ app/src/main/java/com/pbde401/studyworks/ui/candidate/chats/CandidateChatsViewModel.java
@@ -2,5 +2,8 @@ package com.pbde401.studyworks.ui.candidate.chats;
 
 import androidx.lifecycle.ViewModel;
 
+import com.pbde401.studyworks.data.repository.ChatsRepository;
+
 public class CandidateChatsViewModel extends ViewModel {
+    private final ChatsRepository chatsRepository = new ChatsRepository();
 }
