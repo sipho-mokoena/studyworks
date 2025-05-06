19e6428701af6007250944f56c9233da800798d0
sipho-mokoena <siphowilliammokoena@gmail.com>
2025-05-06 20:26:17 +0200

    feat: Add logout functionality to Candidate and Employer main activities with toolbar menu integration

--- app/src/main/java/com/pbde401/studyworks/ui/employer/EmployerMainActivity.java
+++ app/src/main/java/com/pbde401/studyworks/ui/employer/EmployerMainActivity.java
@@ -1,7 +1,9 @@
 package com.pbde401.studyworks.ui.employer;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -10,18 +12,24 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import com.pbde401.studyworks.MainActivity;
 import com.pbde401.studyworks.R;
+import com.pbde401.studyworks.util.AuthManager;
 
 public class EmployerMainActivity extends AppCompatActivity {
     private NavController navController;
     private AppBarConfiguration appBarConfiguration;
+    private AuthManager authManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_main);
 
-        // Set up toolbar if needed
+        // Initialize AuthManager
+        authManager = AuthManager.getInstance();
+
+        // Set up toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -34,16 +42,38 @@ public class EmployerMainActivity extends AppCompatActivity {
 
             navController = navHostFragment.getNavController();
             appBarConfiguration = new AppBarConfiguration.Builder(
-//                    R.id.navigation_employer_dashboard,
-                    R.id.navigation_employer_jobs,
-                    R.id.navigation_employer_applications,
-                    R.id.navigation_employer_chats,
-                    R.id.navigation_employer_profile
+                R.id.navigation_employer_jobs,
+                R.id.navigation_employer_applications,
+                R.id.navigation_employer_chats,
+                R.id.navigation_employer_profile
             ).build();
 
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
             NavigationUI.setupWithNavController(bottomNav, navController);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            handleLogout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void handleLogout() {
+        authManager.logout();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
