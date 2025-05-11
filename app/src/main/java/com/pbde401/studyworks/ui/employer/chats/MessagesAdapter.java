c8fd350818fb413c9c1034fe41970449c4ecb952
sipho-mokoena <siphowilliammokoena@gmail.com>
2025-05-11 02:27:53 +0200

    feat: Implement chat functionality with ChatListAdapter, ChatViewModel, and message handling

---
+++ app/src/main/java/com/pbde401/studyworks/ui/employer/chats/MessagesAdapter.java
@@ -0,0 +1,70 @@
+package com.pbde401.studyworks.ui.employer.chats;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pbde401.studyworks.R;
+import com.pbde401.studyworks.data.models.Message;
+import com.pbde401.studyworks.util.AuthManager;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessageViewHolder> {
+    private List<Message> messages = new ArrayList<>();
+    private final String currentUserId;
+    private static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+    public MessagesAdapter() {
+        this.currentUserId = AuthManager.getInstance().getCurrentUser().getValue().getUid();
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(viewType == 0 ? R.layout.item_message_sent : R.layout.item_message_received, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Message message = messages.get(position);
+        holder.messageText.setText(message.getContent());
+        holder.timeText.setText(timeFormat.format(message.getTimestamp()));
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message message = messages.get(position);
+        return message.getSenderId().equals(currentUserId) ? 0 : 1;
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+        notifyDataSetChanged();
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+        TextView timeText;
+
+        MessageViewHolder(View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+            timeText = itemView.findViewById(R.id.timeText);
+        }
+    }
+}
